f1b934e99caa75d12013abf580fe509a75b04e7e
bryan <bryan.guner@gmail.com>
2021-08-02 07:38:48 -0400

    update

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
-import Container from "./container";
-import { EXAMPLE_PATH } from "../lib/constants";
+import Container from './container'
+import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Footer() {
   return (
@@ -7,17 +7,17 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Blog Home
+            Statically Generated with Next.js.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://bgoonz-blog.netlify.app/"
+              href="https://nextjs.org/docs/basic-features/pages"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Blog Home
+              Read Documentation
             </a>
             <a
-              href={`https://github.com/bgoonz/blog-w-comments`}
+              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
               View on GitHub
@@ -26,5 +26,5 @@ export default function Footer() {
         </div>
       </Container>
     </footer>
-  );
+  )
 }
--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
+import Head from 'next/head'
+import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
   return (
@@ -32,8 +32,11 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta name="description" content={`Bryan Guner Content Blog`} />
+      <meta
+        name="description"
+        content={`A statically generated blog example using Next.js and Sanity.io.`}
+      />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
-  );
+  )
 }
